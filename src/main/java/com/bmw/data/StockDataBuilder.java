4332f891265117c9cd8cddc418e9d710effd25a3
LIANG XIONG <xionglwh@cn.ibm.com>
2019-08-07 16:04:17 +0800

    Add group name, dealer name, region name.

--- src/main/java/com/bmw/data/StockDataBuilder.java
+++ src/main/java/com/bmw/data/StockDataBuilder.java
@@ -18,6 +18,10 @@ public class StockDataBuilder {
 	private static String[] groupIds = new String[] {"GP-10001", "GP-10002", "GP-10003"};
 	private static String[] regionIds = new String[] {"RG-10001", "RG-10002"};
 
+	private static String[] dealerNames = new String[] {"北京京宝行汽车服务有限公司", "北京博瑞宝汽车销售服务有限公司", "深圳博瑞宝汽车销售服务有限公司", "广州博瑞宝汽车销售服务有限公司"};
+	private static String[] groupNames = new String[] {"京宝行集团", "博瑞宝集团", "宝马销售集团"};
+	private static String[] regionNames = new String[] {"华北区", "华南区"};
+
 	private static String[] licensePlateCodes = new String[] {"京A37G03","辽B66778","鄂A33669"};
 
 	private static String[] vehicleSeriesCodes = new String[] {"E60", "E66", "E70", "E46", "E90"};
@@ -39,16 +43,45 @@ public class StockDataBuilder {
 
 		LocalDate now = LocalDate.now();
 		int count = 0;
-		for(String dealerId: dealerIds) {
-
+		for(int m = 0; m < dealerIds.length ; m++) {
+			String dealerId = dealerIds[m];
+			String dealerName = dealerNames[m];
 			for(int i = 0 ; i < 10 ; i++) {
 				count++;
 				Stock stock = new Stock();
 				String id = count < 10 ? "0" + count : String.valueOf(count);
 				stock.setStockId(stockIdPrefix + id);
+				String groupId = null;
+				String regionId = null;
+				String groupName = null;
+				String regionName = null;
+				if(m == 0 || m == 1) {
+					groupId = groupIds[0];
+					regionId = regionIds[0];
+					groupName = groupNames[0];
+					regionName = regionNames[0];
+				}
+
+				if(m == 2) {
+					groupId = groupIds[1];
+					regionId = regionIds[1];
+					groupName = groupNames[1];
+					regionName = regionNames[1];
+				}
+
+				if(m == 3) {
+					groupId = groupIds[2];
+					regionId = regionIds[1];
+					groupName = groupNames[2];
+					regionName = regionNames[1];
+				}
+
 				stock.setDealerId(dealerId);
-				stock.setGroupId(groupIds[count % 3]);
-				stock.setRegionId(regionIds[count % 2]);
+				stock.setGroupId(groupId);
+				stock.setRegionId(regionId);
+				stock.setGroupName(groupName);
+				stock.setRegionName(regionName);
+				stock.setDealerName(dealerName);
 				stock.setVehicleSeriesCode(vehicleSeriesCodes[count % 5]);
 				stock.setVehicleModelCode(vehicleModelCodes[count % 5]);
 				stock.setVehicleModelConfig(vehicleModelConfigs[count % 5]);
